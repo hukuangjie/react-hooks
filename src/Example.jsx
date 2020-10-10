62769375e65e60b68fcd28e853b62d2210183bce
胡旷杰 <754432607@qq.com>
2020-10-10 17:04:45 +0800

    hooks学习 多状态学习

--- src/Example.jsx
+++ src/Example.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-class Example extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (  );
-    }
+import React, { useState } from 'react';
+
+function Example() {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <p>You Clicked {count} times </p>
+            <button onClick={() => { setCount(count + 1) }}>Click me</button>
+        </div>
+    )
 }
 
 export default Example;
