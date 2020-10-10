9a0f7cae33fb34abe3de37165a2dac06cebb5eae
胡旷杰 <754432607@qq.com>
2020-10-10 16:43:28 +0800

    初始化项目

---
+++ src/Example.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Example extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (  );
+    }
+}
+
+export default Example;
